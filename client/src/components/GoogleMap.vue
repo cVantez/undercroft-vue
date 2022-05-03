c43798a1a7adedb9be4b9d01aa464fae22eee891
cVantez <peterh.barna@gmail.com>
2022-05-02 21:25:10 -0400

    Add place info to GoogleMap integration

--- client/src/components/GoogleMap.vue
+++ client/src/components/GoogleMap.vue
@@ -21,45 +21,77 @@ export default {
     }
   },
   watch: {
+    // this is in a watch expression because the API key won't be loaded
+    // from the back end yet during mounted()
     apiKey(newVal) {
       this.$nextTick(async () => {
-        // lat/long of the undercroft
-        const location = {
-          lat: 39.32022635593587,
-          lng: -76.62091036297952
-        };
-
         const loader = new Loader({
           apiKey: newVal,
           version: 'weekly',
           libraries: ['places']
         });
 
-        const mapOptions = {
-          center: location,
+        const { maps } = await loader.load();
+        const map = new maps.Map(document.getElementById('uc-map'), {
+          // lat/long of the undercroft
+          center: {
+            lat: 39.32022635593587,
+            lng: -76.62091036297952
+          },
           zoom: 15
-        };
-
-        const g = await loader.load();
-        const map = new g.maps.Map(document.getElementById('uc-map'), mapOptions);
-        const infoWindow = new g.maps.InfoWindow();
-
-        // TODO: do this using places for better info
-        const marker = new g.maps.Marker({
-          position: location,
-          map,
-          title: 'The Undercroft',
-          label: 'The Undercroft',
-          optimized: false
         });
 
-        // accessibility for map marker
-        marker.addListener('click', () => {
-          infoWindow.close();
-          infoWindow.setContent(marker.getTitle());
-          infoWindow.open(marker.getMap(), marker);
+        const infoWindow = new maps.InfoWindow();
+        const service = new maps.places.PlacesService(map);
+
+        service.getDetails({
+          // placeID of The Undercroft
+          placeId: 'ChIJcUOPJwcFyIkRcX94id64dCU',
+          fields: ['formatted_address', 'geometry', 'name', 'url'],
+        }, (place, status) => {
+          if (
+            status === maps.places.PlacesServiceStatus.OK
+            && place
+            && place.geometry
+            && place.geometry.location
+          ) {
+            const marker = new maps.Marker({
+              map,
+              position: place.geometry.location,
+            });
+
+            maps.event.addListener(marker, 'click', () => {
+              this.showPlaceInfo(map, place, marker, infoWindow);
+            });
+          }
         });
       });
+    }
+  },
+  methods: {
+    showPlaceInfo(map, place, marker, infoWindow) {
+      const content = document.createElement('div');
+      content.className = 'p-1 text-left';
+
+      const nameEl = document.createElement('p');
+      nameEl.className = 'text-one font-weight-bold mb-1';
+      nameEl.textContent = place.name;
+      content.appendChild(nameEl);
+
+      const addressEl = document.createElement('p');
+      addressEl.className = 'mb-1';
+      addressEl.textContent = place.formatted_address;
+      content.appendChild(addressEl);
+
+      const linkEl = document.createElement('a');
+      linkEl.className = 'text-cyan';
+      linkEl.href = place.url;
+      linkEl.target = '_blank';
+      linkEl.textContent = 'Open in Google Maps';
+      content.appendChild(linkEl);
+
+      infoWindow.setContent(content);
+      infoWindow.open(map, marker);
     }
   }
 };
--- server/config/googleConfig.js
+++ server/config/googleConfig.js
@@ -1,7 +1,7 @@
 // To avoid committing sensitive information, we must read from a local config.
 // Create a file with the indicated name in this directory that exports the apiKey.
 // Get your own API key here: https://console.cloud.google.com/google/maps-apis/credentials
-// $300 free trial / $7 per 1000 page loads / $ 200 free monthly
+// $300 free trial / $24 per 1000 page loads ($7 maps + $17 places) + / $ 200 free monthly
 
 const local = require('#config/googleConfig.local');
 
