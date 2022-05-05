6a947f4becd19e36059444620ff547e97b59e27d
cVantez <peterh.barna@gmail.com>
2022-05-04 21:47:11 -0400

    Fix loading bug with FbEvents
    Previously if the page was not the first page loaded, the appId would already
    have been loaded, so the watch expression would not trigger.
    Add a mounted hook that runs if appId is set and non-empty.
    Move adding window.fbAsyncInit to a function inside a check if that function
    already exists.

--- client/src/components/FbEvents.vue
+++ client/src/components/FbEvents.vue
@@ -21,6 +21,11 @@
 import { mapState } from 'vuex';
 
 export default {
+  data() {
+    return {
+      isFbAlreadyLoaded: false
+    };
+  },
   computed: {
     ...mapState('meta', ['links']),
     appId() {
@@ -29,26 +34,43 @@ export default {
   },
   watch: {
     // this is in a watch expression because the API key won't be loaded
-    // from the back end yet during mounted()
+    // from the back end yet during the first mounted()
     appId(newVal) {
-      // set callback for when FB SDK is loaded
-      window.fbAsyncInit = () => {
-        window.FB.init({
-          appId: newVal,
-          autoLogAppEvents: true,
-          xfbml: true,
-          version: 'v13.0'
-        });
-      };
+      this.addFbAsyncInit(newVal);
       this.loadFbSdk();
     }
   },
+  mounted() {
+    // will run if this is not the first page loaded
+    // i.e. appId()
+    if (this.appId && this.appId !== '') {
+      this.addFbAsyncInit(this.appId);
+      this.loadFbSdk();
+      if (this.isFbAlreadyLoaded) {
+        window.fbAsyncInit();
+      }
+    }
+  },
   methods: {
+    // set callback for when FB SDK is loaded
+    addFbAsyncInit(id) {
+      if (!window.fbAsyncInit) {
+        window.fbAsyncInit = () => {
+          window.FB.init({
+            appId: id,
+            autoLogAppEvents: true,
+            xfbml: true,
+            version: 'v13.0'
+          });
+        };
+      }
+    },
     // add FB SDK to page scripts if it isnt already there
     loadFbSdk() {
       const id = 'fb-js-sdk';
       const firstScript = document.getElementsByTagName('script')[0];
       if (document.getElementById(id)) {
+        this.isFbAlreadyLoaded = true;
         return;
       }
 
@@ -56,8 +78,7 @@ export default {
       fbScript.id = id;
       fbScript.src = 'https://connect.facebook.net/en_US/sdk.js';
       firstScript.parentNode.insertBefore(fbScript, firstScript);
-    },
-
+    }
   }
 };
 </script>
